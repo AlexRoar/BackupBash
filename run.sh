3d5cec009e744ab7dfa9717d5884503e5aeae998
alexdremov <dremov.me@gmail.com>
2022-02-21 14:41:16 +0300

    Backup dir creation and re-creation, walker function blueprint's done

--- run.sh
+++ run.sh
@@ -1,27 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # ./run.sh ~/repo cpp backup backup.tar.gz
-# ./run.sh <абсолютный путь> <расширение> <название папки для бекапа> <имя архива с бекапом>
+# ./run.sh 1:<абсолютный путь> 2:<расширение> 3:<название папки для бекапа> 4:<имя архива с бекапом>
 
 copy_dir_files() { # (1:directory, 2:targetDirectory, 3:fileExtension
- # echo "$1"
- local files=`ls -1a $1`
- IFS='
+  # echo "$1"
+  local files
+  files=$(ls -1a $1)
+  IFS='
  '
- for var in $files
- do
-  if [ "$var" == "." ]; then
-   continue
-  fi
-  if [ "$var" == ".." ]; then
-   continue
-  fi
-  echo $var
+  for var in $files; do
+    if [ "$var" == "." ]; then
+      continue
+    fi
+    if [ "$var" == ".." ]; then
+      continue
+    fi
 
-  if [ -d "$var" ]; then
-   walk_dir "$(pwd)/$1/$var"
-  fi
- done
+    if [ -d "$1/$var" ]; then
+      copy_dir_files "$1/$var" "$2/$var" "$3"
+    else
+      echo "From $1/$var to $2/$var"
+    fi
+  done
 }
+
+if [ -d "$3" ]; then
+  rm -rf "$3"
+  mkdir "$3"
+else
+  mkdir "$3"
+fi
 
 copy_dir_files "$1" "$(pwd)/$3" "$2"
